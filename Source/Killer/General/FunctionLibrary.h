51e4ec448636268e08db257492d02bc0852e5864
Suzuya <raachkovskiy9339@gmail.com>
2023-02-03 14:30:08 +0300

    LevelsMenu

--- Source/Killer/General/FunctionLibrary.h
+++ Source/Killer/General/FunctionLibrary.h
@@ -14,7 +14,7 @@ class KILLER_API UFunctionLibrary : public UObject
 	
 public:
 	static void SpawnParticlesAndSound(UWorld* World, UNiagaraSystem* NiagaraSystem, USoundWave* SoundWave,
-		const FVector& Location, const FRotator& Rotation = FRotator(), bool RandomizePitch = true);
+		const FVector& Location, const FRotator& Rotation = FRotator(0.0f, 0.0f, 0.0f), bool RandomizePitch = true);
 
 	UFUNCTION(BlueprintCallable)
 		static USave* GetSave();
